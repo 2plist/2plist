0b4a4fc6987da97d6f52bfd9b9aa6dcfe191bb28
Motor8gd <129219670+Motor8gd@users.noreply.github.com>
2023-04-16 20:01:46 -0700

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -186,9 +186,9 @@ export default {
                 video: lvl.verification,
             }));
             const list = [];
-            if (this.useMainList) list.push(...fullListMapped.slice(0, 75));
+            if (this.useMainList) list.push(...fullListMapped.slice(0, 15));
             if (this.useExtendedList) {
-                list.push(...fullListMapped.slice(75, 150));
+                list.push(...fullListMapped.slice(15, 50));
             }
 
             // random 100 levels
