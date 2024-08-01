718d5e1612bb9e030e8c5561c8fb38fbe8836688
Motor8gd <129219670+Motor8gd@users.noreply.github.com>
2024-08-01 15:46:57 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 50) {
+    if (rank > 75) {
         return 0;
     }
-    if (rank > 15 && percent < 100) {
+    if (rank > 25 && percent < 100) {
         return 0;
     }
 
