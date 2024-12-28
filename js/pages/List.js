1457c4ed6ef08358f37fccd6a58ccbc0ed863282
gdbatle <129358280+gdbatle@users.noreply.github.com>
2024-12-28 15:36:46 +0000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,8 +25,9 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
-                            <p v-else class="type-label-lg" style="color: gray"> Legay </p>
+                            <p v class="type-label-lg">#{{ i + 1 }}</p>
+                            <! -- <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p> -->
+                            <! -- <p v-else class="type-label-lg" style="color: gray"> Legay </p> -->
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
